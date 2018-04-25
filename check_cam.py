a7f848a003572ee3be23aa29274a54d03c158eca
fghdot25 <fghdot25@gmail.com>
2018-04-25 17:18:26 +0600

    first commit

---
+++ check_cam.py
@@ -0,0 +1,21 @@
+
+""" Для проверки работоспособности камеры!!! """
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ training.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import sys
+
+from classifier import training
+import config
+
+datadir = './faces_dir'
+modeldir = './model/20170511-185253.pb'
+classifier_filename = './class/classifier.pkl'
+print('Training start')
+obj = training(datadir, modeldir, classifier_filename)
+get_file = obj.main_train()
+print('Saved classifier model to file "%s' % get_file)
+sys.exit("All done!")
